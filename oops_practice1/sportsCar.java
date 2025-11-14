076109e87d0e30c9e5bc255ddd65f196088a120f
vikram17dp <vikramdp505@gmail.com>
2025-11-14 11:38:32 +0530

    new commit

--- oops_practice1/sportsCar.java
+++
@@ -1,9 +0,0 @@
-package oops_practice;
-
-public class sportsCar extends Car {
-    public sportsCar(String brand, String model) {
-        super(brand, model);
-    }
-
-    // You can override methods if needed
-}
